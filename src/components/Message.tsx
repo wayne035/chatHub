575704c2be5058f52c9a729ba5dce9f4cc25d8b9
DESKTOP-R2NP3JG\user <wayne30317@gmail.com>
2023-06-09 15:44:00 +0800

    增加message發訊時間功能

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 interface Message {
   fromSelf: boolean;
   message: string;
+  currentTime?:string;
 }
 
 export default function Message({currentChat,self,socket}:any) {
@@ -29,20 +30,32 @@ export default function Message({currentChat,self,socket}:any) {
 
   const sendMessage = (msg:string)=>{
 
+    const currentTime = () =>{
+      const time = new Date()
+      const month = (1 + time.getMonth()).toString().padStart(2,"0")
+      const year = time.getFullYear().toString().padStart(2,"0")
+      const day = time.getDate().toString().padStart(2,"0")
+      const hour = time.getHours().toString().padStart(2,"0")
+      const minute = time.getMinutes().toString().padStart(2,"0")
+      return `${year}/${month}/${day} ${hour}:${minute}`
+    }
+
     socket.current.emit('sendMsg',{
       from :self['id'],
       to:currentChat._id,
       msg,
+      currentTime:currentTime(),
     })
 
     axios.post('http://localhost:8000/api/userdata/addmsg',{
       from :self['id'],
       to:currentChat._id,
       message:msg,
+      currentTime:currentTime(),
     })
 
     const msgs = [...messages]
-    msgs.push({fromSelf : true, message : msg})
+    msgs.push({fromSelf : true, message : msg, currentTime:currentTime()})
     setMessages(msgs)
   }
 
@@ -71,8 +84,12 @@ export default function Message({currentChat,self,socket}:any) {
           return(
             <div ref={scrollRef} key={uuidv4()} className='border-2 w-full '>
               <div className={`${msg['fromSelf']?'text-right':'text-left'}`}>
-                {msg['message']}
+                <p className ='text-[18px] font-bold'>{msg['message']}</p>
+                <span className='text-[13px] font-bold text-[#777]'>
+                  {msg['currentTime']}
+                </span>
               </div>
+              
             </div>
           )
         })
